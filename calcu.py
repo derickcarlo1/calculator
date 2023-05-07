512a780a1d47a53227d83d02ea80b317cb7166d8
Derick Carlo S. Herrera <dericktzy@gmail.com>
2023-05-07 10:29:49 +0800

    ask the user for two numbers and operation

--- calcu.py
+++ calcu.py
@@ -2,7 +2,12 @@ import operator
 
 # define a function for performing the calculations
 def calculate():
-
+    try:
+        # ask the user for two numbers and an operation
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        operation = input("Choose an operation (+, -, *, /): ")
+        
         # ask the user for two numbers and an operation
         
         # perform the calculation based on the chosen operation
