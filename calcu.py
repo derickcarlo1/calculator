1081a8eb39489ce6fa0665a0c99f10065a9434cb
Derick Carlo S. Herrera <dericktzy@gmail.com>
2023-05-07 10:26:12 +0800

    plotting pseudocodes

---
+++ calcu.py
@@ -0,0 +1,13 @@
+# define a function for performing the calculations
+
+        # ask the user for two numbers and an operation
+        
+        # perform the calculation based on the chosen operation
+
+        # print the result of the calculation
+        
+        # handle errors when the user enters invalid input
+        
+        # handle errors when the user tries to divide by zero
+
+# repeatedly ask the user to perform calculations
